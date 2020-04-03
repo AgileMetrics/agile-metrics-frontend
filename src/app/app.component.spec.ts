9adde60e97a98d0ca6b28f9dd5460747d3afa137
Carlos Camacho <ccamachog@gmail.com>
2020-04-03 20:29:26 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('agile-metrics-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('agile-metrics-frontend deployed in github pages!!!!');
   });
 });
