3330d6ea0ac1fcab5ba0b66361287319c7411e19
RoyNexus <pacorg@gmail.com>
2020-09-20 17:44:35 +0200

    ng lint fixed

--- src/app/pages/cycletime/component/cycletime.component.ts
+++ src/app/pages/cycletime/component/cycletime.component.ts
@@ -1,86 +1,89 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import CycleTimeService from '../service/cycletime.service';
 import * as Highcharts from 'highcharts';
 
 @Component({
-    selector: 'app-cycletime-component',
-    templateUrl: './cycletime.component.html',
-    styleUrls: ['./cycletime.component.css']
+  selector: 'app-cycletime-component',
+  templateUrl: './cycletime.component.html',
+  styleUrls: ['./cycletime.component.css']
 })
 export class CycleTimeComponent implements OnInit, OnDestroy {
 
-    constructor(private cycleTimeService: CycleTimeService) {
-        console.log('construct highchart component');
-    }
-    ngOnInit() {
-        console.log('init highchart component');
-        this.cycleTimeService.getCycleTimeForScatterPlot().subscribe((scatterData: Array<any>) => {
-            this.scatterChart(scatterData);
-        });
-    }
-    scatterChart(scatterData: Array<any>) {
-        Highcharts.chart('scatter-container', {
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Cycle Time Scatterplot'
-            },
-            xAxis: {
-                title: {
-                    text: 'Completion Date',
-                },
-                showFirstLabel: true,
-                showLastLabel: true,
-                startOnTick: true,
-                endOnTick: true,
-                type: 'datetime',
-                dateTimeLabelFormats: {
-                    day: '%e-%b-%y',
-                    month: '%b-%y'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Days',
+  constructor(private cycleTimeService: CycleTimeService) {
+    console.log('construct highchart component');
+  }
 
-                }
-            },
-            series: [
-                {
-                    type: 'scatter',
-                    name: 'Cycle Time',
-                    color: 'rgba(40, 40, 255, .6)',
-                    data: scatterData
-                }
-            ],
-            plotOptions: {
-                scatter: {
-                    marker: {
-                        radius: 6,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-                    tooltip: {
-                        headerFormat: '<b>{series.name}</b><br>',
+  ngOnInit() {
+    console.log('init highchart component');
+    this.cycleTimeService.getCycleTimeForScatterPlot().subscribe((scatterData: Array<any>) => {
+      this.scatterChart(scatterData);
+    });
+  }
 
-                        pointFormatter() {
-                            const dateFormatted = Highcharts.dateFormat('%d-%m-%Y', this.x);
-                            return `Completion Date: ${dateFormatted} <br> Cycle Time: ${this.y} days`;
-                        }
-                    }
+  scatterChart(scatterData: Array<any>) {
+    Highcharts.chart('scatter-container', {
+      chart: {
+        type: 'scatter',
+        zoomType: 'xy'
+      },
+      title: {
+        text: 'Cycle Time Scatterplot'
+      },
+      xAxis: {
+        title: {
+          text: 'Completion Date',
+        },
+        showFirstLabel: true,
+        showLastLabel: true,
+        startOnTick: true,
+        endOnTick: true,
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          day: '%e-%b-%y',
+          month: '%b-%y'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Days',
 
-                }
-            },
-        }, null);
+        }
+      },
+      series: [
+        {
+          type: 'scatter',
+          name: 'Cycle Time',
+          color: 'rgba(40, 40, 255, .6)',
+          data: scatterData
+        }
+      ],
+      plotOptions: {
+        scatter: {
+          marker: {
+            radius: 6,
+            states: {
+              hover: {
+                enabled: true,
+                lineColor: 'rgb(100,100,100)'
+              }
+            }
+          },
+          tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
 
-    }
-    ngOnDestroy() {
-        console.log('destroy highchart component');
-    }
+            pointFormatter() {
+              const dateFormatted = Highcharts.dateFormat('%d-%m-%Y', this.x);
+              return `Completion Date: ${dateFormatted} <br> Cycle Time: ${this.y} days`;
+            }
+          }
+
+        }
+      },
+    }, null);
+
+  }
+
+  ngOnDestroy() {
+    console.log('destroy highchart component');
+  }
 }
--- src/app/pages/cycletime/service/cycletime.service.ts
+++ src/app/pages/cycletime/service/cycletime.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
-import * as moment from 'moment'
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
